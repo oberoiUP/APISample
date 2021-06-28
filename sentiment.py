de6cca45a7e68393f8905c1d5525021e38945f8e
Daman Oberoi <daman.oberoi@seo-usa.org>
2021-06-28 19:46:28 +0000

    updated style

--- sentiment.py
+++ sentiment.py
@@ -5,5 +5,5 @@ text = input("Enter text: ")
 url = 'http://text-processing.com/api/sentiment/'
 myobj = {'text': text}
 
-response = requests.post(url, data = myobj)
+response = requests.post(url, data=myobj)
 print(response.json())

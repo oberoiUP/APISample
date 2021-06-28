6e647c2d02ab919700f0ec8810196afecaeac6db
Daman Oberoi <daman.oberoi@seo-usa.org>
2021-06-28 19:42:55 +0000

    Added space to comment and removed newline at EOF

--- weatherAPI.py
+++ weatherAPI.py
@@ -8,11 +8,10 @@ url = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q='
 url += city + '&appid=' + APIkey
 
 response = requests.get(url)
-#print(response.json())
+# print(response.json())
 
 weatherList = response.json()
 main = weatherList['main']
 
 print("Current Temp: {}".format(main['temp']))
 print("Feels Like: {}".format(main['feels_like']))
-

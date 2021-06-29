8bb3154990345d77dfa01b2da8b5b88f982e7fb4
Daman Oberoi <daman.oberoi@seo-usa.org>
2021-06-29 21:27:58 +0000

    fixed style issues

--- test_weatherAPI.py
+++ test_weatherAPI.py
@@ -1,14 +1,21 @@
 import unittest
 from weatherAPI_functions import build_url, get_json
 
+
 class TestFileName(unittest.TestCase):
     def test_build_url(self):
-        self.assertEqual(build_url('Portland', '0b9ec521e0c913cd98a5430c6fae3baf'), 'https://api.openweathermap.org/data/2.5/weather?units=imperial&q=Portland&appid=0b9ec521e0c913cd98a5430c6fae3baf')
-    
+        url = 'https://api.openweathermap.org/data/2.5/weather?' + \
+          'units=imperial&q=Portland&appid=0b9ec521e0c913cd98a5430c6fae3baf'
+        self.assertEqual(build_url('Portland',
+                                   '0b9ec521e0c913cd98a5430c6fae3baf'), url)
+
     def test_get_json(self):
-        json = get_json('https://api.openweathermap.org/data/2.5/weather?units=imperial&q=Portland&appid=0b9ec521e0c913cd98a5430c6fae3baf')
+        url = 'https://api.openweathermap.org/data/2.5/weather?' + \
+          'units=imperial&q=Portland&appid=0b9ec521e0c913cd98a5430c6fae3baf'
+        json = get_json(url)
         self.assertNotEqual(json, None)
         self.assertTrue(json['name'] == 'Portland')
-    
+
+
 if __name__ == '__main__':
     unittest.main()

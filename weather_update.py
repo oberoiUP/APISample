b26d71f8b759ca4915d9b9f510be408bd1bd1ad7
oberoiUP <45634895+oberoiUP@users.noreply.github.com>
2021-07-01 19:44:40 -0700

    Fixed style 3

--- weather_update.py
+++ weather_update.py
@@ -39,55 +39,57 @@ def create_engine_function(dbName):
 
 def write_table(dataframe, dbName, tableName):
     os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS '
-	+ dbName + '; "')
-    dataframe.to_sql(tableName, con=create_engine_function(dbName), 
-	if_exists='replace',
-    index=False)
+              + dbName + '; "')
+    dataframe.to_sql(tableName, con=create_engine_function(dbName),
+                     if_exists='replace',
+                     index=False)
 
 
 def save_data_to_file(dataframe, dbName, tableName, fileName):
-    dataframe.to_sql(tableName, con=create_engine_function(dbName), if_exists='replace',
+    dataframe.to_sql(tableName, con=create_engine_function(dbName),
+                     if_exists='replace',
                      index=False)
-    os.system('mysqldump -u root -pcodio {} > {}.sql'.format(dbName, fileName))    
+    os.system('mysqldump -u root -pcodio {} > {}.sql'.format(dbName, fileName))
 
 
 def load_database(dbName, fileName):
-    os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS '+dbName+'; "')
+    os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS '
+              + dbName + '; "')
     os.system('mysql -u root -pcodio ' + dbName + ' < ' + fileName + '.sql')
 
 
 def update_database(dbName, tableName, fileName):
     load_database(dbName, fileName)
     # write_table(dataframe, dbName, tableName, 'append')
-    df = pd.read_sql_table(tableName, con=create_engine_function(dbName))    
+    df = pd.read_sql_table(tableName, con=create_engine_function(dbName))
     return df
 
 
 def main():
-  
+
     database = 'weather'
     table = 'weather_data'
     filename = 'dump'
-  
+
     df = update_database(database, table, filename)
-  
+
     city, apiKey = gather_input()
     url = build_url(city, apiKey)
     json = get_json(url)
-    
+
     df.loc[len(df.index)] = [datetime.datetime.now(),
                              json['name'],
                              json['main']['temp'],
                              json['main']['feels_like']]
-    # df = build_dataframe(json)
-    # df = pd.read_sql_table('weather_data', con=create_engine_function('weather'))
+
     write_table(df, database, table)
-    
+
     save_data_to_file(df, database, table, filename)
-    
+
     # manual record inserts
     # os.system('mysql -u root -pcodio -e "USE weather;"'))
-    # os.system('mysql -u root -pcodio -e "INSERT DATABASE IF NOT EXISTS '+database_name+'; "')
+    # os.system('mysql -u root -pcodio -e "INSERT DATABASE IF NOT EXISTS '
+    # + database_name + '; "')
 
 
 if __name__ == "__main__":
